797a23b3f6dea0126a9fd2200e42085d76f63b2e
Spencer Smith <smithspencer817@gmail.com>
2020-05-05 01:58:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,14 @@
 def join_nested_strings(src)
-  
+  a=0 
+  result=[]
+  while a < src.length do
+    b=0
+    while b < src[a].length do
+      if src[a][b].class == String
+        result << src[a][b]
+      b+=1
+    end
+    a+=1
+  end
+  result.join()
 end
